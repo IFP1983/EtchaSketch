75fd07b71ef5b8188f93b92539e089384536c84f
Isaiah Prince <prince1819386@lcsd2kids.org>
2021-04-21 11:03:43 -0600

    Got it working

---
+++ main.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('happy');
+content.textContent = 'This is the beginning';
+
+container.appendChild(content);
+
+const paragraph = document.createElement('p');
+paragraph.classList.add('red');
+paragraph.textContent = 'Hey I am red';
+
+container.appendChild(paragraph);
+
+const text = document.createElement('h3');
+text.classList.add('happy');
+text.textContent = 'I am a blue h3';
+
+container.appendChild(text);
+
+const text = document.createElement('h3');
+text.classList.add('happy');
+text.textContent = 'I am a blue h3';
+
+container.appendChild(text);
