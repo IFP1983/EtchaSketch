d023d3667cd5e1dc00d3d453f539d613c67a32a4
Isaiah Prince <prince1819386@lcsd2kids.org>
2021-05-06 11:21:05 -0600

    Got the erase working

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const gridContainer = document.querySelector("#grid-container");
 
-let numOfCells = 256;
-for(let i = 0;i<numOfCells;i++) {
-    const singleCell = document.createElement("div");
-    singleCell.textContent = "";
-    singleCell.style.color = "white";
-    singleCell.classList.add("bg-red")
-    singleCell.background = "black";
-    singleCell.addEventListener("mouseover", () => {
-        singleCell.classList.add("mousedOver");
-    });
-    gridContainer.appendChild(singleCell);
+function drawGrid(numOfCells){
+    numOfCells = numOfCells**2;
+    for(let i = 0;i<numOfCells;i++) {
+        const singleCell = document.createElement("div");
+        singleCell.textContent = "";
+        singleCell.style.color = "white";
+        singleCell.classList.add("bg-red")
+        singleCell.background = "black";
+        singleCell.addEventListener("mouseover", () => {
+            singleCell.classList.add("mousedOver");
+        });
+        gridContainer.appendChild(singleCell);
+    }
+    
 }
+const eraser = document.querySelector("#erase");
+eraser.addEventListener('click', () => {
+    gridContainer.innerHTML = "";
+    drawGrid(prompt("Dimension: "));
+});
 
-
+drawGrid(16);
